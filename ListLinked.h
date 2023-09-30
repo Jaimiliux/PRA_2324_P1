52eb20120fffd679afcbe145a75ddb3d5759cdc9
Jaime Martinez Pico <jmarpic@alumno.upv.es>
2023-09-30 16:41:27 +0200

    Añadido código de test de la clase ListLinked; Makefile actualizado :)

--- ListLinked.h
+++ ListLinked.h
@@ -38,29 +38,27 @@ class ListLinked : public List<T> {
 		     }
 		     void insert(int pos, T e){
 			     if(pos >= 0 && pos <= n){
-				     switch (pos){
-					     case 0:
+				     if(pos == 0){
 	                                        first = new Node(e,first->next);
-						break;
-					     case size():
+				     }
+				     if(pos == n){
 						Node<T>* aux = first;
 					        Node<T>* preaux = aux;
 						for(int i = 0; i < pos; i++){
 						     preaux = aux;
 						     aux = aux->next;
 						}
-						aux = new Node(e, nullptr);
+						aux = new Node(e);
 						preaux->next = aux;
-						break;
-                                             default:
-				     	        aux = first;
-				     	        preaux = aux;
+				     }
+                                     if(pos != 0 && pos != n){
+				     	        Node<T>* aux = first;
+				     	        Node<T>* preaux = aux;
 				     	     	for(int i = 0; i < pos; i++){
 					     	     preaux = aux;
 					     	     aux = aux->next;
 				     	     	}
 				     	     	preaux->next = new Node(e, aux);
-					     	break;
 				     }
 		                     n++;
 			     }
@@ -71,49 +69,47 @@ class ListLinked : public List<T> {
 		     void append(T e){
 			     insert(n, e);
 		     }
-		     void prepend(T e ){
+		     void prepend(T e){
 			     insert(0, e);
 		     }
 		     T remove(int pos){
 			     if(pos >= 0 && pos < n){
-				     switch (pos){
-					  case 0:
+				     if(pos == 0){
 				     		Node<T>* aux = first;
 						first = aux->next;
 						T num = aux->data;
 						delete aux;
+						n--;
 						return num;
-						break;
-					  case size()-1:
-					        aux = first;
+				     }
+				     if(pos == n-1){
+					        Node<T>* aux = first;
 					        Node<T>* preaux = aux;
 				     		for(int i = 0; i < pos; i++){
 							preaux = aux;
 							aux = aux->next;
 						}
 						preaux->next = nullptr;
-						num = aux->data;
+						T num = aux->data;
 						delete aux;
+						n--;
 						return num;
-						break;
-					  default:
-						aux = first;
-						preaux = aux;
+				     }
+				     if(pos != 0 && pos != n-1){
+						Node<T>* aux = first;
+						Node<T>* preaux = aux;
 						for(int i = 0;i < pos; i++){
 							preaux = aux;
 							aux = aux->next;
 						}
 						preaux->next = aux->next;
-						num = aux->data;
+						T num = aux->data;
 						delete aux;
+						n--;
 						return num;
-						break;
 				     }
-				     n--;
 			     }
-			     else{
 				     throw std::out_of_range("Fuera de rango");
-			     }
 		     }
 		     T get(int pos){
 			     if(pos >= 0 && pos < n){
